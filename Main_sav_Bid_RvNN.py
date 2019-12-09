5cd93fd9cee5ebed9c937bc940fe5e5ac1920c1e
Seunghee-Koh <seunghee1215@kaist.ac.kr>
2019-12-09 11:43:10 +0900

    SAVE version added.

---
+++ Main_sav_Bid_RvNN.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+"""
+@object: Twitter
+@task: Main function of recursive NN (4 classes)
+@author: majing
+@structure: bottom-up recursive neural networks
+@variable: Nepoch, lr, obj, fold
+@time: Jan 24, 2018
+"""
+
+import sys
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+import os
+import sav_Bid_RvNN as Bid_RvNN
+import TD_RvNN
+import BU_RvNN
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+
+import time
+import datetime
+import random
+from evaluate import *
+#from Util import *
+
+obj = "Twitter15" # choose dataset, you can choose either "Twitter15" or "Twitter16"
+fold = "2" # fold index, choose from 0-4
+tag = "_u2b"
+vocabulary_size = 5000
+hidden_dim = 100
+Nclass = 4
+Nepoch = 500
+lr = 0.005
+
+treePath = '../resource/data.TD_RvNN.vol_'+str(vocabulary_size)+'.txt' 
+#treePath_bu = '../resource/data.BU_RvNN.vol_'+str(vocabulary_size)+'.txt' 
+
+trainPath = "../nfold/RNNtrainSet_"+obj+str(fold)+"_tree.txt" 
+testPath = "../nfold/RNNtestSet_"+obj+str(fold)+"_tree.txt"
+labelPath = "../resource/"+obj+"_label_All.txt"
+
+
+
+#floss = open(lossPath, 'a+')
+
+################################### tools #####################################
+def str2matrix(Str, MaxL): # str = index:wordfreq index:wordfreq
+    wordFreq, wordIndex = [], []
+    l = 0
+    for pair in Str.split(' '):
+        wordFreq.append(float(pair.split(':')[1]))
+        wordIndex.append(int(pair.split(':')[0]))
+        l += 1
+    ladd = [ 0 for i in range( MaxL-l ) ]
+    wordFreq += ladd 
+    wordIndex += ladd 
+    #print MaxL, l, len(Str.split(' ')), len(wordFreq)
+    #print Str.split(' ')
+    return wordFreq, wordIndex 
+
+def loadLabel(label, l1, l2, l3, l4):
+    labelset_nonR, labelset_f, labelset_t, labelset_u = ['news', 'non-rumor'], ['false'], ['true'], ['unverified']
+    if label in labelset_nonR:
+       y_train = [1,0,0,0]
+       l1 += 1
+    if label in labelset_f:
+       y_train = [0,1,0,0] 
+       l2 += 1
+    if label in labelset_t:
+       y_train = [0,0,1,0] 
+       l3 += 1 
+    if label in labelset_u:
+       y_train = [0,0,0,1] 
+       l4 += 1
+    return y_train, l1,l2,l3,l4
+
+def constructTree(tree, tree_type):
+    ## tree: {index1:{'parent':, 'maxL':, 'vec':}
+    ## 1. ini tree node
+    index2node = {}
+    for i in tree:
+        if tree_type == 0:
+            node = TD_RvNN.Node_tweet(idx=i)
+        else :
+            node = BU_RvNN.Node_tweet(idx=i)
+        index2node[i] = node
+    ## 2. construct tree
+    for j in tree:
+        indexC = j 
+        indexP = tree[j]['parent']
+        nodeC = index2node[indexC]
+        wordFreq, wordIndex = str2matrix( tree[j]['vec'], tree[j]['maxL'] )
+        #print tree[j]['maxL']
+        nodeC.index = wordIndex
+        nodeC.word = wordFreq
+        #nodeC.time = tree[j]['post_t']
+        ## not root node ## 
+        if not indexP == 'None':
+           nodeP = index2node[int(indexP)]
+           nodeC.parent = nodeP
+           nodeP.children.append(nodeC)
+        ## root node ##
+        else:
+           root = nodeC
+    ## 3. convert tree to DNN input    
+    parent_num = tree[j]['parent_num'] 
+    ini_x, ini_index = str2matrix( "0:0", tree[j]['maxL'] )
+    if tree_type ==0:
+        x_word, x_index, tree = TD_RvNN.gen_nn_inputs(root, ini_x) 
+    else:
+        x_word, x_index, tree = BU_RvNN.gen_nn_inputs(root, only_leaves_have_vals=False) 
+    return x_word, x_index, tree, parent_num
+
+################################# loas data ###################################
+def loadData():
+    print ("loading tree label"),
+    labelDic = {}
+    for line in open(labelPath):
+        line = line.rstrip()
+        label, eid = line.split('\t')[0], line.split('\t')[2]
+        labelDic[eid] = label.lower()   
+    print( len(labelDic))
+
+    print( "reading tree", )## X
+    treeDic = {}
+    for line in open(treePath):
+        line = line.rstrip()
+        eid, indexP, indexC = line.split('\t')[0], line.split('\t')[1], int(line.split('\t')[2])
+        parent_num, maxL = int(line.split('\t')[3]), int(line.split('\t')[4])  
+        Vec =  line.split('\t')[5] 
+        if treeDic.get(eid) is None:
+           treeDic[eid] = {}
+        treeDic[eid][indexC] = {'parent':indexP, 'parent_num':parent_num, 'maxL':maxL, 'vec':Vec}   
+    print( 'tree no:', len(treeDic))
+
+    # for i,(eid, data) in enumerate(treeDic.items()):
+    #     i += 1
+    #     print('--------------------')
+    #     print(eid)
+    #     print(data[0][1]['parent_num'],len(data[0]))
+    #     if i >= 10:
+    #         break
+    print( "loading train set", )
+    tree_train, word_train, index_train, y_train, parent_num_train, c = [], [], [], [], [], 0
+    l1,l2,l3,l4 = 0,0,0,0
+    for eid in open(trainPath):
+        #if c > 8: break
+        eid = eid.rstrip()
+        if labelDic.get(eid) is None: continue
+        if treeDic.get(eid) is None: continue 
+        if len(treeDic[eid]) <= 0: 
+           #print labelDic[eid]
+           continue
+        ## 1. load label
+        label = labelDic[eid]
+        y, l1,l2,l3,l4 = loadLabel(label, l1, l2, l3, l4)
+        y_train.append(y)
+        ## 2. construct tree
+        #print eid
+        x_word_td, x_index_td, tree_td, parent_num = constructTree(treeDic[eid], 0)
+        x_word_bu, x_index_bu, tree_bu, _ = constructTree(treeDic[eid], 1)
+        tree_train.append([tree_td,tree_bu])
+        word_train.append([x_word_td,x_word_bu])
+        index_train.append([x_index_td, x_index_bu])
+        parent_num_train.append(parent_num)
+        #print treeDic[eid]
+        #print tree, child_num
+        #exit(0)
+        c += 1
+    print( l1,l2,l3,l4)
+
+    print( "loading test set", )
+    tree_test, word_test, index_test, parent_num_test, y_test, c = [], [], [], [], [], 0
+    l1,l2,l3,l4 = 0,0,0,0
+    for eid in open(testPath):
+        #if c > 4: break
+        eid = eid.rstrip()
+        if labelDic.get(eid) is None: continue
+        if treeDic.get(eid) is None: continue
+        if len(treeDic[eid]) <= 0: 
+            #print labelDic[eid] 
+            continue
+        ## 1. load label        
+        label = labelDic[eid]
+        y, l1,l2,l3,l4 = loadLabel(label, l1, l2, l3, l4)
+        y_test.append(y)
+        ## 2. construct tree
+        x_word_td, x_index_td, tree_td, parent_num = constructTree(treeDic[eid], 0)
+        x_word_bu, x_index_bu, tree_bu, _ = constructTree(treeDic[eid], 1)
+        tree_test.append([tree_td,tree_bu])
+        word_test.append([x_word_td,x_word_bu])
+        index_test.append([x_index_td, x_index_bu])
+        parent_num_test.append(parent_num)
+        c += 1
+    print( l1,l2,l3,l4)
+    print( "train no:", len(tree_train), len(word_train), len(index_train),len(parent_num_train), len(y_train))
+    print( "test no:", len(tree_test), len(word_test), len(index_test), len(parent_num_test), len(y_test))
+    print("dim1 for 0:", len(tree_train[0][0]), len(word_train[0][0]), len(index_train[0][0]))
+    #print("case 0:", tree_train[0][0], word_train[0][0], index_train[0][0], parent_num_train[0])
+    #print index_train[0]
+    #print word_train[0]
+    #print tree_train[0]    
+    #exit(0)
+    return tree_train, word_train, index_train, parent_num_train, y_train, tree_test, word_test, index_test, parent_num_test, y_test
+
+##################################### MAIN ####################################        
+## 1. load tree & word & index & label
+tree_train, word_train, index_train, parent_num_train, y_train, tree_test, \
+                word_test, index_test, parent_num_test, y_test = loadData()
+
+## 1.5. Check device and get device (gpu, cpu)
+device='cpu'
+print(device)
+## 2. ini RNN model
+t0 = time.time()
+
+def get_model_path(last_epoch, model_dir):
+    return model_dir + '/model_' + str(last_epoch) + '.pkl'
+
+model_dir = './Bid_models'
+if not os.path.isdir(model_dir):
+    os.mkdir(model_dir)
+if len(sys.argv) >1:
+    last_epoch = int(sys.argv[1])
+    load_model_path = get_model_path(last_epoch, model_dir)
+else:
+    last_epoch = 0
+    load_model_path = None
+
+
+if load_model_path is not None and os.path.exists(load_model_path):
+    print('Load model : %s'%load_model_path)
+    model = torch.load(load_model_path)
+else:
+    model = Bid_RvNN.RvNN(vocabulary_size, hidden_dim, Nclass, device=device)
+
+t1 = time.time()
+print('Recursive model established,', (t1-t0)/60)
+
+
+######################
+
+## 3. looping SGD
+losses_5, losses = [], []
+num_examples_seen = 0
+for epoch in range(last_epoch + 1, Nepoch):
+    ## one SGD 
+    indexs = [i for i in range(len(y_train))]
+    random.shuffle(indexs) 
+    for i in indexs:
+        #print i,
+        #print(parent_num_train)
+
+        loss, pred_y = model.forward(word_train[i], index_train[i], parent_num_train[i], tree_train[i], y_train[i], lr)
+        #print loss, pred_y
+        losses.append(float(loss))
+        num_examples_seen += 1
+    print( "epoch=%d: loss=%.4f" % ( epoch, np.mean(losses) ))
+    #floss.write(str(time)+": epoch="+str(epoch)+" loss="+str(loss) +'\n')
+    sys.stdout.flush()
+    
+    ## cal loss & evaluate
+    if epoch % 5 == 0:
+       save_path = get_model_path(epoch, model_dir)
+       torch.save(model, save_path)
+       print('Save model at [%s]'%save_path)
+       losses_5.append((num_examples_seen, np.mean(losses))) 
+       time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+       print( "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, np.mean(losses)))
+       #floss.write(str(time)+": epoch="+str(epoch)+" loss="+str(loss) +'\n') 
+       #floss.flush()        
+       sys.stdout.flush()
+       prediction = []
+       for j in range(len(y_test)):
+           #print j
+           prediction.append(model.predict_up(word_test[j], index_test[j], parent_num_test[j], tree_test[j]) )   
+       res = evaluation_4class(prediction, y_test) 
+       print( 'results:', res)
+       #floss.write(str(res)+'\n')
+       #floss.flush() 
+       sys.stdout.flush()
+       ## Adjust the learning rate if loss increases
+       if len(losses_5) > 1 and losses_5[-1][1] > losses_5[-2][1]:
+          lr = lr * 0.5   
+          print( "Setting learning rate to %f" % lr)
+          #floss.write("Setting learning rate to:"+str(lr)+'\n')
+          #floss.flush() 
+          sys.stdout.flush()
+       #save_model_Recursive_gruEmb(modelPath, model)   
+    sys.stdout.flush()
+    losses = []
---
+++ sav_Bid_RvNN.py
@@ -0,0 +1,253 @@
+__doc__ = """Tree GRU aka Recursive Neural Networks."""
+
+import numpy as np
+#import theano
+#from theano import tensor as T
+#from collections import OrderedDict
+#from theano.compat.python2x import OrderedDict
+#from theano.tensor.signal.pool import pool_2d
+import torch
+import torch.nn.functional as F
+
+dtype = 'float32'
+torch_dtype = torch.float32
+torch_dtype_d = torch.float64
+
+def hard_sigmoid(x):
+    """
+    Computes element-wise hard sigmoid of x.
+    See e.g. https://github.com/Theano/Theano/blob/master/theano/tensor/nnet/sigm.py#L279
+    """
+    x = (0.2 * x) + 0.5
+    x = F.threshold(-x, -1, -1)
+    x = F.threshold(-x, 0, 0)
+    return x
+
+################################ tree rnn class ######################################
+class RvNN(object):
+    """Data is represented in a tree structure.
+
+    Every leaf and internal node has a data (provided by the input)
+    and a memory or hidden state.  The hidden state is computed based
+    on its own data and the hidden states of its children.  The
+    hidden state of leaves is given by a custom init function.
+
+    The entire tree's embedding is represented by the final
+    state computed at the root.
+
+    """
+    def __init__(self, word_dim, hidden_dim=5, Nclass=4,
+                degree=2, momentum=0.9,
+                 trainable_embeddings=True,
+                 labels_on_nonroot_nodes=False,
+                 irregular_tree=True,
+                 device='cpu'):
+        assert word_dim > 1 and hidden_dim > 1
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.Nclass = Nclass
+        self.degree = degree
+        self.momentum = momentum
+        self.irregular_tree = irregular_tree
+        self.device=device
+        self.params = []
+
+        torch.autograd.set_detect_anomaly(True)
+
+        self.create_output_fn()
+        self.create_td_recursive_unit()
+        self.create_bu_recursive_unit()
+        
+
+    def forward(self, x_word, x_index, num_parent, tree,y,lr):
+        td_final_state = self.td_compute_tree(x_word[0], x_index[0], num_parent, tree[0])
+        bu_final_state = self.bu_compute_tree(x_word[1], x_index[1], tree[1])
+        #print(td_final_state.shape,bu_final_state.shape)
+
+        final_state = torch.cat((td_final_state, bu_final_state), dim=0)
+        pred_y = self.output_fn(final_state)
+        loss = self.loss_fn(y, pred_y)
+        self.gradient_descent(loss, lr)
+
+        return loss, pred_y.tolist()
+
+    def predict_up(self, x_word, x_index, num_parent, tree):
+        # similar to forward function.
+        # except loss, gradient part.\
+        td_final_state = self.td_compute_tree(x_word[0], x_index[0], num_parent, tree[0])
+        bu_final_state = self.bu_compute_tree(x_word[1], x_index[1], tree[1])
+        final_state = torch.cat((td_final_state, bu_final_state), dim=0)
+        pred_y = self.output_fn(final_state)
+        return pred_y.tolist()
+
+    def init_matrix(self, shape):
+        return np.random.normal(scale=0.1, size=shape).astype(dtype)
+
+    def init_vector(self, shape):
+        return np.zeros(shape, dtype=dtype)
+
+    def create_output_fn(self):
+        self.W_out = torch.tensor(self.init_matrix([self.Nclass, self.hidden_dim*2]), requires_grad = True, device=self.device)
+        self.b_out = torch.tensor(self.init_vector([self.Nclass]),requires_grad = True, device=self.device)
+        self.params.extend([self.W_out, self.b_out])
+
+    def output_fn(self, final_state):
+        return F.softmax(torch.matmul(self.W_out, final_state)+
+                         self.b_out, dim=0)
+
+    def create_td_recursive_unit(self):
+        self.E_td = torch.tensor(self.init_matrix([self.hidden_dim, self.word_dim]),requires_grad = True, device=self.device)
+        self.W_z_td = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.U_z_td = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.b_z_td = torch.tensor(self.init_vector([self.hidden_dim]),requires_grad = True, device=self.device)
+        self.W_r_td = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.U_r_td = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.b_r_td = torch.tensor(self.init_vector([self.hidden_dim]),requires_grad = True, device=self.device)
+        self.W_h_td = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.U_h_td = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.b_h_td = torch.tensor(self.init_vector([self.hidden_dim]),requires_grad = True, device=self.device)
+        self.params.extend([self.E_td, self.W_z_td, self.U_z_td, self.b_z_td, self.W_r_td, self.U_r_td, \
+                self.b_r_td, self.W_h_td, self.U_h_td, self.b_h_td])
+        
+    def td_recursive_unit(self, child_word, child_index, parent_h):
+        child_xe = self.E_td[:, child_index].mul(torch.tensor(child_word, device=self.device)).sum(dim=1)
+        z_td = hard_sigmoid(self.W_z_td.mul(child_xe).sum(dim=1) + self.U_z_td.mul(parent_h).sum(dim=1) + self.b_z_td)
+        r_td = hard_sigmoid(self.W_r_td.mul(child_xe).sum(dim=1) + self.U_r_td.mul(parent_h).sum(dim=1) + self.b_r_td)
+        c = torch.tanh(self.W_h_td.mul(child_xe).sum(dim=1) + self.U_h_td.mul(parent_h * r_td).sum(dim=1) + self.b_h_td)
+        h_td = z_td * parent_h + (1 - z_td) * c
+        return h_td
+
+    def td_compute_tree(self, x_word, x_index, num_parent, tree):
+        self.num_nodes = x_word.shape[0]
+        node_h = torch.tensor(self.init_vector([self.num_nodes, self.hidden_dim]), requires_grad = True, device=self.device)
+
+        # use recurrence to compute internal node hidden states
+        def _recurrence(x_word, x_index, node_info, node_h):
+            parent_h = node_h[node_info[0]]
+            child_h = self.td_recursive_unit(x_word, x_index, parent_h)
+            node_h = torch.cat([node_h[:node_info[1]],
+                                    child_h.reshape([1, self.hidden_dim]),
+                                    node_h[node_info[1]+1:]])
+            return node_h, child_h
+
+        child_hs = []
+        for x_word_i, x_index_i, tree_i in zip(x_word, x_index, tree) :
+            #print(tree_i)
+            node_h, child_hs_i =_recurrence(x_word_i, x_index_i, tree_i, node_h)
+            child_hs.append(child_hs_i.reshape(1, -1))
+        return torch.cat(child_hs[num_parent-1:], dim=0).max(dim=0).values
+    """
+    def td_compute_tree(self, x_word, x_index, num_parent, tree):
+        print(num_parent)
+        num_nodes = x_word.shape[0]
+        node_h = torch.tensor(self.init_vector([num_nodes, self.hidden_dim]), requires_grad = True, device=self.device)
+
+        # use recurrence to compute internal node hidden states
+        def _recurrence(x_word, x_index, node_info, node_h):
+            parent_h = node_h[node_info[0]]
+            child_h = self.td_recursive_unit(x_word, x_index, parent_h)
+            node_h[node_info[1]] = child_h
+            # node_h = torch.cat([node_h[:node_info[1]],
+            #                         child_h.reshape([1, self.hidden_dim]),
+            #                         node_h[node_info[1]+1:]])
+            return node_h#, child_h
+
+#        child_hs = []
+        #node_h = init_node_h
+        for x_word_i, x_index_i, tree_i in zip(x_word, x_index, tree) :
+            #print(tree_i)
+            node_h =_recurrence(x_word_i, x_index_i, tree_i, node_h)
+        #    child_hs.append(child_hs_i.reshape(1, -1))
+        #print(child_hs[0].shape, len(child_hs[num_parent-1:]),num_parent,len(child_hs))
+        # return torch.cat(child_hs[num_parent-1:], 0).max(dim=0).values
+        #print(node_h.shape,node_h[num_parent:].shape, num_nodes,num_parent)
+        #print(torch.max(node_h[num_parent:],dim=0).values.type(torch_dtype))
+        return torch.max(node_h[num_parent:],dim=0).values
+
+    """
+    def leaf_unit(self, leaf_word, leaf_index):
+        dummy = torch.zeros([self.degree, self.hidden_dim], dtype=torch_dtype, device=self.device)
+        return self.bu_recursive_unit( leaf_word, leaf_index, dummy)
+
+    def create_bu_recursive_unit(self):
+        self.E_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.word_dim]),requires_grad = True, device=self.device)
+        self.W_z_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.U_z_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.b_z_bu = torch.tensor(self.init_vector([self.hidden_dim]),requires_grad = True, device=self.device)
+        self.W_r_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.U_r_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.b_r_bu = torch.tensor(self.init_vector([self.hidden_dim]),requires_grad = True, device=self.device)
+        self.W_h_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.U_h_bu = torch.tensor(self.init_matrix([self.hidden_dim, self.hidden_dim]),requires_grad = True, device=self.device)
+        self.b_h_bu = torch.tensor(self.init_vector([self.hidden_dim]),requires_grad = True, device=self.device)
+        self.params.extend([self.E_bu, self.W_z_bu, self.U_z_bu, self.b_z_bu, self.W_r_bu, self.U_r_bu, self.b_r_bu, \
+                self.W_h_bu, self.U_h_bu, self.b_h_bu])
+        
+    def bu_recursive_unit(self, parent_word, parent_index, child_h):
+        h_tilde = child_h.sum(dim=0)
+        parent_xe = self.E_bu[:, parent_index].mul(torch.tensor(parent_word, device=self.device)).sum(dim=1)
+        z_bu = hard_sigmoid(self.W_z_bu.mul(parent_xe).sum(dim=1) + self.U_z_bu.mul(h_tilde).sum(dim=1) + self.b_z_bu)
+        r_bu = hard_sigmoid(self.W_r_bu.mul(parent_xe).sum(dim=1) + self.U_r_bu.mul(h_tilde).sum(dim=1) + self.b_r_bu)
+        c = torch.tanh(self.W_h_bu.mul(parent_xe).sum(dim=1) + self.U_h_bu.mul(h_tilde * r_bu).sum(dim=1) + self.b_h_bu)
+        h_bu = z_bu * h_tilde + (1 - z_bu) * c
+        return h_bu
+
+    def bu_compute_tree(self, x_word, x_index, tree):
+        num_nodes = x_word.shape[0]
+        num_parents = tree.shape[0]  # num internal nodes
+        num_leaves = num_nodes - num_parents
+
+        # compute leaf hidden states
+        leaf_h= torch.stack([self.leaf_unit(w, i) for w, i in zip(x_word[:num_leaves], x_index[:num_leaves])], dim=0)
+        init_node_h = leaf_h
+
+        # use recurrence to compute internal node hidden states
+        def _recurrence(x_word, x_index, tree, idx, node_h):
+            #node_h means node's hidden state (start from only leaf and their upper node are added)
+            # tree size is num_leaves
+            child_exists = (tree[:-1] > -1).nonzero()
+            # maybe child_h means (tree>-1)th nodes are children of parent(x_word, x_index)
+            child_h = node_h[tree[child_exists]] 
+            # pass all children of one parent to parent node as hidden state
+            parent_h = self.bu_recursive_unit(x_word, x_index, child_h) # parent node's hidden state
+            node_h = torch.cat((node_h, parent_h.view(1, -1)), 0) # add parent node to input
+            return node_h, parent_h
+
+        node_h = init_node_h # only leaf nodes
+        # for num_parent step running 
+        for idx, (w, x, t) in enumerate(zip(x_word[num_leaves:], x_index[num_leaves:], tree)):
+            #node_h means node's hidden state (start from only leaf and their upper node are added)
+            #print("BU",t)
+            node_h, parent_h=_recurrence(w, x, t, idx, node_h)
+
+        #exit()
+        return parent_h.type(torch_dtype)
+
+    def loss_fn(self, y, pred_y):
+
+        return torch.sum((torch.tensor(y, device=self.device) - pred_y).pow(2))
+
+
+    def gradient_descent(self, loss, lr):
+        """Momentum GD with gradient clipping."""
+        with torch.autograd.set_detect_anomaly(True):
+            loss.backward()
+
+        grad = [p.grad for p in self.params]
+        self.momentum_velocity_ = [0.] * len(grad)
+        grad_norm = torch.sqrt(sum(map(lambda x: x.pow(2).sum(), grad)))
+        not_finite = torch.isnan(grad_norm)|torch.isinf(grad_norm)
+        scaling_den = max(5.0, grad_norm)
+        for n, (param, grad_i) in enumerate(zip(self.params, grad)):
+            param.requires_grad = False
+            if not_finite :
+                grad_i = 0.1 * param
+            else :
+                grad_i = grad_i * (5.0 / scaling_den)
+            velocity = self.momentum_velocity_[n]
+            update_step = self.momentum * velocity - lr * grad_i
+            self.momentum_velocity_[n] = update_step
+            param.add_(update_step)
+            param.requires_grad = True
+            param.grad.zero_()
+            # param = param + update_step
